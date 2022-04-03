822c9cde50ef6bb5732057e4e2b8da29091ea66f
zandro <lamdimoko@gmail.com>
2022-04-03 11:47:06 +0800

    useRef issue

--- anpanmoneyv1/src/index.js
+++ anpanmoneyv1/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-import App from './App'
+import App from './components/App'
 
 reactDom.render(
     <BrowserRouter>
