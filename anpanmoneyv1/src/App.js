dafa953be7cbe9fdb75c8d8237b918dee86118cb
sent <krieton1204@gmail.com>
2022-04-03 19:36:11 +0800

    abc

--- anpanmoneyv1/src/App.js
+++ anpanmoneyv1/src/App.js
@@ -8,41 +8,24 @@ import Settings from "./components/pages/Settings";
 import Login from './components/Login'
 import Register from './components/Register'
 
-function App(){
-    const[registerEmail, setRegisterEmail] = useState(" ");
-    const[registerPassword, setRegisterPassword] = useState(" ");
-    const[loginEmail, setLoginEmail] = useState(" ");
-    const[loginPassword, setLoginPassword] = useState(" ");
-
-    const register = async() => {
-
-    }
-    const login = async() => {
+const App = () => {
+    return(
     
-    }
-    
-    const logout = async() => {
-    
-    }
-    
-    const App = () => {
-        return(
+        <div className="app-main">
             
-        
-            <div className="app-main">
-                <Routes>
-                    <Route path='/login' element={<Login/>} />
-                    <Route path='/home' element={<Home/>} />
-                    <Route path='/register' element={<Register/>} />
-                    <Route path='/dashboard' element={<Dashboard/>} />
-                    <Route path='/profile' element={<Profile/>} />
-                    <Route path='/settings' element={<Settings/>} />
-                </Routes>
-            </div>
-        
-        
-        )
-}
+            <Routes>
+
+                <Route path='/login' element={<Login/>} />
+                <Route path='/home' element={<Home/>} />
+                <Route path='/register' element={<Register/>} />
+                <Route path='/dashboard' element={<Dashboard/>} />
+                <Route path='/profile' element={<Profile/>} />
+                <Route path='/settings' element={<Settings/>} />
+            </Routes>
+        </div>
+    
+    
+    )
 }
 
 export default App
--- anpanmoneyv1/src/index.js
+++ anpanmoneyv1/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-import App from './components/App'
+import App from './App'
 
 reactDom.render(
     <BrowserRouter>
